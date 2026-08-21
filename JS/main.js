43f7a02fe7f507276db780b3036de44c84cd47b7
draberty <draberty1@gmail.com>
2026-08-21 10:44:32 -0400

    Attempt 3

--- JS/main.js
+++ JS/main.js
@@ -1,15 +1,15 @@
 // sw register
 if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-      navigator.serviceWorker.register("../sw.js")
-        .then((registration) => {
-          console.log('ServiceWorker registered with scope:', registration.scope);
-        })
-        .catch((error) => {
-          console.error('ServiceWorker registration failed:', error);
-        });
-    });
-  }
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('./sw.js')
+      .then((registration) => {
+        console.log('ServiceWorker registered with scope:', registration.scope);
+      })
+      .catch((error) => {
+        console.error('ServiceWorker registration failed:', error);
+      });
+  });
+}
 
 let currentSelectedMeals = "";
 
